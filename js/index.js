69c5648be339a69d7d833e63ec4f20e318457316
rkskek8484-cell <rkskek8484@gmail.com>
2026-05-26 17:11:05 +0900

    feat:add modal

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const popup = document.querySelector("#popup");
+const popupCloseBtn = popup.querySelector("button");
+const agree = document.querySelector("#agree");
+
+if (document.cookie.includes("popup=닫기")) {
+  popup.close();
+} else {
+  popup.showModal();
+}
+
+popupCloseBtn.addEventListener("click", () => {
+  popup.close();
+  if (agree.checked) {
+    createCookie("popup", "닫기", 1);
+  } else {
+    createCookie("popup", "닫기", -1);
+  }
+});
+
+function createCookie(name, value, expire) {
+  let today = new Date();
+  today.setDate(today.getDate() + expire);
+  document.cookie = `${name}=${value}; Expires=${today.toString()}`;
+}
